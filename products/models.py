98a0deb1fae70c9ad05b40b57d132ea5a44825fe
nidhu23 <nidhunanik@gmail.com>
2020-10-12 23:03:57 +0530

    [Nidhuna]add: Product model created

--- products/models.py
+++ products/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 # Create your models here.
-
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    image = models.CharField(max_length=500)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=20, decimal_places=3)
+    description = models.CharField(max_length=1000)
