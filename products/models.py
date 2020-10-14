3b6fb3ce68feec6d3d39321ae8ec245110377021
nidhu23 <nidhunanik@gmail.com>
2020-10-14 14:18:44 +0530

    [Nidhuna]refactor: type of field price changed from decimal to float field

--- products/models.py
+++ products/models.py
@@ -7,5 +7,5 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     image = models.CharField(max_length=500)
     quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=20, decimal_places=3)
+    price = models.FloatField()
     description = models.CharField(max_length=10000)
