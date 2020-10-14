07146183a279dcb7ea2a2b234feaeba873b25104
nidhu23 <nidhunanik@gmail.com>
2020-10-14 14:21:52 +0530

    [Nidhuna]add: url pattern added for the sort_by_price view

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("allbooks/", views.get_books),
     path("bynameorauthor/", views.book_by_title_or_author),
+    path("sortbyprice/", views.sort_by_price),
 ]
