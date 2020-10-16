36356e4067489b4ce0b0c0acdc299dfb33bf3f15
nidhu23 <nidhunanik@gmail.com>
2020-10-16 14:40:10 +0530

    [Nidhuna]refactor: doc string for API updated

--- products/views.py
+++ products/views.py
@@ -16,16 +16,19 @@ logger = logging.getLogger("django")
 @api_view(["GET"])
 def get_books(request, sort="", id=0):
     """
-    API to get all the books available
+    API to get all the books available or
+    book corresponding to an id, author or title of book
 
     Parameter:
     argument(1): value for sort(name of field to be sorted by)
     argument(2): id to search book details corresponding to it
+    argument(3): request body with either title or author of book
 
     Returns:
     Details of all books available sorted by field name given,
     by default returns books details by insertion order or
-    details of book corresponding to the id passed.
+    details of book corresponding to the id or title or author
+    name passed.
     """
     try:
         books = get_book(request, id, sort)
