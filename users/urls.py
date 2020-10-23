6b39c75febebd2f794697f1b118a5b671b176e23
nidhu23 <nidhunanik@gmail.com>
2020-10-23 13:17:04 +0530

    [Nidhuna]add: url pattern for verify_otp ang logout API

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("login/", views.login)]
+urlpatterns = [
+    path("login/", views.login),
+    path("login/verify/", views.verify_otp),
+    path("logout/", views.logout),
+]
