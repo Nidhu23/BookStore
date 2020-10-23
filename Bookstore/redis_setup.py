01f80b3c45489131b075fbe074523453a7700747
nidhu23 <nidhunanik@gmail.com>
2020-10-23 13:00:21 +0530

    [Nidhuna]refactor: redis connection done in class instead of function

--- Bookstore/redis_setup.py
+++ Bookstore/redis_setup.py
@@ -1,6 +1,34 @@
 import redis
-from Bookstore import settings
+from Bookstore import settings, BookstoreError, responses
+from rest_framework import status
 
 
-def get_redis_instance():
-    return redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0)
+class CacheSetUp:
+    def __init__(self):
+        self.redis_instance = redis.Redis(
+            host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=0
+        )
+
+    def set_value(self, key, value):
+        try:
+            self.redis_instance.set(key, value)
+        except redis.exceptions.ConnectionError:
+            raise BookstoreError.BookStoreError(
+                responses.get_response("RedisConnectionError"), 500
+            )
+
+    def get_value(self, key):
+        try:
+            return self.redis_instance.get(key).decode()
+        except redis.exceptions.ConnectionError:
+            raise BookstoreError.BookStoreError(
+                responses.get_response("RedisConnectionError"), 500
+            )
+
+    def del_value(self, key):
+        try:
+            return self.redis_instance.delete(key)
+        except redis.exceptions.ConnectionError:
+            raise BookstoreError.BookStoreError(
+                responses.get_response("RedisConnectionError"), 500
+            )
