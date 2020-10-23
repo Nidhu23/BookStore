3115a1e16d97eb0362143b9c8538f31efca315c2
nidhu23 <nidhunanik@gmail.com>
2020-10-23 13:10:50 +0530

    [Nidhuna]add: API to verify otp and logout API

--- users/views.py
+++ users/views.py
@@ -1,22 +1,31 @@
-from django.shortcuts import render
 from django.db.models import Q
 from rest_framework.decorators import api_view
-from rest_framework import request
+from rest_framework import request, status
 from rest_framework.response import Response
 from .models import User
 from .serializers import LoginSerializer
 from .otp import send_otp
-from Bookstore import redis_setup
-from rest_framework import status
+from Bookstore import redis_setup, responses, BookstoreError
 import logging
+from .services import get_tokens, delete_user
 
+cache_instance = redis_setup.CacheSetUp()
 logger = logging.getLogger("django")
 # Create your views here.
 @api_view(["POST"])
 def login(request):
+    """
+    API for user login
+
+    Parameter:
+    argument(1): request method with request data containing either
+    user name or mobile number
+
+    Returns:
+    sends user verification otp
+    """
     try:
-        redis_instance = redis_setup.get_redis_instance()
-        username = request.data.get("user_name")
+        username = request.data.get("username")
         mobile_num = request.data.get("mobile_num")
         if username == None and mobile_num == None:
             return Response("Please Enter either user name or mobile number for login")
@@ -26,19 +35,16 @@ def login(request):
             if serializer.is_valid:
                 if serializer.data == []:
                     return Response(
-                        {
-                            "message": "user with this number or name does not exist",
-                            "status": status.HTTP_404_NOT_FOUND,
-                        },
-                        status=status.HTTP_404_NOT_FOUND,
+                        responses.get_response("UserNotExist"),
+                        status=responses.get_response("UserNotExist").get("status"),
                     )
                 mobile_num = serializer.data.get("mobile_num")
                 username = serializer.data.get("username")
                 OTP = send_otp(mobile_num)
-                redis_instance.set("otp_" + username, OTP)
+                cache_instance.set_value("otp_" + username, OTP)
                 return Response(
-                    {"message": "OTP sent successfully", "status": status.HTTP_200_OK},
-                    status=status.HTTP_200_OK,
+                    responses.get_response("OtpSuccess"),
+                    status=responses.get_response("OtpSuccess").get("status"),
                 )
             else:
                 return Response(
@@ -50,18 +56,94 @@ def login(request):
                 )
     except User.DoesNotExist:
         return Response(
-            {
-                "message": "user with this number or name does not exist",
-                "status": status.HTTP_404_NOT_FOUND,
-            },
-            status=status.HTTP_404_NOT_FOUND,
+            responses.get_response("UserNotExist"),
+            status=responses.get_response("UserNotExist").get("status"),
         )
+    except BookstoreError.BookStoreError as error:
+        return Response(error.message, error.status)
     except Exception as e:
         logger.error(e)
         return Response(
-            {
-                "message": "Something went wrong",
-                "status": status.HTTP_500_INTERNAL_SERVER_ERROR,
-            },
-            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            responses.get_response("GenericError"),
+            status=responses.get_response("GenericError").get("status"),
+        )
+
+
+@api_view(["POST"])
+def verify_otp(request):
+    """
+    API to verify otp
+
+    Parameter:
+    argument(1): request method with request data containing user name
+    and otp
+
+    Returns:
+    verifies otp and sends tokens in response header
+    """
+    try:
+        username = request.data.get("username")
+        otp = request.data.get("otp")
+        if username == None or otp == None:
+            return Response(
+                responses.get_response("OtpOrNameMissing"),
+                status=responses.get_response("OtpOrNameMissing").get("status"),
+            )
+        generated_otp = cache_instance.get_value("otp_" + username)
+        if otp == generated_otp:
+            if cache_instance.del_value("otp_" + username) == 1:
+                tokens = get_tokens(username, cache_instance)
+                return Response(
+                    responses.get_response("LoginSuccess"),
+                    headers={
+                        "access_token": str(tokens.access_token),
+                        "refresh": str(tokens),
+                    },
+                    status=responses.get_response("LoginSuccess").get("status"),
+                )
+        else:
+            return Response(
+                responses.get_response("WrongOtp"),
+                status=responses.get_response("WrongOtp").get("status"),
+            )
+    except BookstoreError.BookStoreError as error:
+        return Response(error.message, error.status)
+    except Exception as e:
+        logger.error(e)
+        return Response(
+            responses.get_response("GenericError"),
+            status=responses.get_response("GenericError").get("status"),
+        )
+
+
+@api_view(["GET"])
+def logout(request):
+    """
+    API to log user out
+
+    Parameter:
+    argument(1): request method with request header containing refresh token
+
+    Returns:
+    logged out message
+    """
+    try:
+        token = request.headers.get("token")
+        if token == None:
+            return Response(
+                responses.get_response("TokenMissing"),
+                status=responses.get_response("TokenMissing").get("status"),
+            )
+        delete_user(token, cache_instance)
+        return Response(
+            responses.get_response("LogoutSuccess"),
+            status=responses.get_response("LogoutSuccess").get("status"),
+        )
+    except BookstoreError.BookStoreError as error:
+        return Response(error.message, error.status)
+    except Exception as e:
+        logger.error(e)
+        return Response(
+            responses.get_response("GenericError"),
+            status=responses.get_response("GenericError").get("status"),
         )
