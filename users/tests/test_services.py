e445d8b041e63a002439b8419c02c9ac91cacfe5
nidhu23 <nidhunanik@gmail.com>
2020-10-23 13:21:45 +0530

    [Nidhuna]add: test case to check if custom exception is thrown when redis connection error occurs

--- users/tests/test_services.py
+++ users/tests/test_services.py
@@ -1,8 +1,15 @@
 import pytest
 from users.otp import send_otp
+from Bookstore.redis_setup import CacheSetUp
 from Bookstore.BookstoreError import BookStoreError
 
 
 def test_givenPhoneNumber_WhenWrongFormat_raisesException():
     with pytest.raises(BookStoreError):
         assert send_otp(+91809557)
+
+
+def test_RedisConnection_whenServerNotRunning_raisesException():
+    cache_instance = CacheSetUp()
+    with pytest.raises(BookStoreError):
+        cache_instance.set_value("key", "value")
